c3de30c5b782de9cdafc1f693851917897ce2bc3
Elvis Okumu <okothelvis137@gmail.com>
2025-10-30 13:32:14 +0300

    Add user model with schema definition.

---
+++ api/modals/userModal.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+    },
+    photo: String,
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- api/server.js
+++ api/server.js
@@ -15,20 +15,6 @@ mongoose
     console.error("Database connection error:", err);
   });
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-});
-
-const User = mongoose.model("User", userSchema);
-
-try {
-  User.create({ name: "John Havvey", email: "john@example.com" });
-  console.log("User created successfully");
-} catch (error) {
-  console.error("Error creating user:", error);
-}
-
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
