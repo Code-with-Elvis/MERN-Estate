8ebfb743f3e516b71e2664dc9e5ab4ac36618725
Elvis Okumu <okothelvis137@gmail.com>
2025-10-30 23:42:40 +0300

    Install and configure tanstack query.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,4 +2,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      cacheTime: 1000 * 60 * 10, // 10 minutes
+    },
+  },
+});
+
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
