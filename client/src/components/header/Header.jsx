eba7821b1b0ed60260c65b64b717a8105aa28e70
Elvis Okumu <okothelvis137@gmail.com>
2025-10-31 15:08:38 +0300

    Fix header styling by removing redundant font weight class from navigation links.

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
       <div className="container relative h-full flex items-center justify-between">
         {/* Header content right */}
         <SmallNav />
-        <div className="hidden sm:flex items-center space-x-7 text-sm font-semibold uppercase">
+        <div className="hidden sm:flex items-center space-x-7 text-sm  uppercase">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/listings">Listings</Link>
