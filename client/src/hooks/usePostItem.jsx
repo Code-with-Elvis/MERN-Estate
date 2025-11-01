57cbbd20b115c29f0c60c3209b8756de3bd34030
Elvis Okumu <okothelvis137@gmail.com>
2025-11-01 21:36:00 +0300

    Add usePostItem hook for posting items.

---
+++ client/src/hooks/usePostItem.jsx
@@ -0,0 +1,46 @@
+import { useAuth } from "@/store";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+const usePostItem = (apiUrl, queryKey, message) => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const logout = useAuth((state) => state.logout);
+
+  const { mutate: postItem, isPending } = useMutation({
+    mutationFn: async (data) => {
+      return axios.post(apiUrl, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryKey] });
+      toast.success(message || "Item posted successfully");
+    },
+    onError: (error) => {
+      console.error("Error:", error?.message || "An error occurred");
+
+      if (error?.response?.status === 401) {
+        queryClient.removeQueries();
+        navigate("/login");
+        logout();
+        toast.error(
+          "You are not authorized to perform this action. Please log in."
+        );
+      } else {
+        const message =
+          error?.response?.data?.message ||
+          "Failed to login. Please try again.";
+        toast.error(message);
+      }
+    },
+  });
+
+  return { postItem, isPending };
+};
+export default usePostItem;
