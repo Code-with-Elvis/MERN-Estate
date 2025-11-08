44f9bcb584a80f3ab8e91d696f5293b308fa2a24
Elvis Okumu <okothelvis137@gmail.com>
2025-11-08 13:57:37 +0300

    Start creating listings page.

---
+++ client/src/components/Listings/Filter.jsx
@@ -0,0 +1,4 @@
+const Filter = () => {
+  return <aside className="w-70 sticky top-0 bg-gray-100 p-4">Filter</aside>;
+};
+export default Filter;
--- client/src/pages/Listings.jsx
+++ client/src/pages/Listings.jsx
@@ -1,4 +1,14 @@
+import Filter from "@/components/Listings/Filter";
+
 const Listings = () => {
-  return <div>Listings</div>;
+  return (
+    <section className="pb-14">
+      <div className="hidden sm:block h-12 bg-[#222]"></div>
+      <div className="container pt-6 flex items-start">
+        <Filter />
+        <div className="flex-1 p-4">Listings Page Content</div>
+      </div>
+    </section>
+  );
 };
 export default Listings;
