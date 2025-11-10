a26b2f842a0681d2cf8a88c148ffa165b1aa390a
Elvis Okumu <okothelvis137@gmail.com>
2025-11-10 10:40:49 +0300

    Add FeaturedListings and RecentListings components to Home page.

---
+++ client/src/components/home/RecentListings.jsx
@@ -0,0 +1,78 @@
+import useGetItems from "@/hooks/useGetItems";
+import { useAuth } from "@/store";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import ListingCard from "./ListingCard";
+
+const RecentListings = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const logout = useAuth((state) => state.logout);
+
+  const { data, error, isPending } = useGetItems(
+    "/api/v1/listings/recent",
+    "listings"
+  );
+
+  useEffect(() => {
+    if (error) {
+      if (error.response?.status === 401) {
+        queryClient.removeQueries();
+        navigate("/login");
+        logout();
+        toast.warning("You are not authorized to perform this action.");
+      } else if (error.response?.status === 403) {
+        navigate("/");
+      } else if (error.response?.status === 404) {
+        navigate("/");
+      }
+    }
+  }, [error, queryClient, navigate, logout]);
+
+  if (isPending) {
+    return (
+      <section className="pb-4">
+        <div className="container">
+          <h2 className="font-extrabold uppercase text-sm mb-4">
+            Recent Listings
+          </h2>
+          <div className="mt-10">Loading ...</div>
+        </div>
+      </section>
+    );
+  }
+  if (error) {
+    return (
+      <section className="pb-4">
+        <div className="container">
+          <h2 className="font-extrabold uppercase text-sm mb-4">
+            Recent Listings
+          </h2>
+          <div className=" pt-10">
+            <CommonError message="Failed to load listings. Please try again later." />
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  const listings = data.data.listings;
+
+  return (
+    <section className="pb-4">
+      <div className="container">
+        <h2 className="font-extrabold uppercase text-sm mb-4">
+          Recent Listings
+        </h2>
+        <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          {listings.map((listing) => (
+            <ListingCard key={listing._id} {...listing} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+export default RecentListings;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import FeaturedListings from "@/components/home/FeaturedListings";
 import FindInterest from "@/components/home/FindInterest";
 import Landing from "@/components/home/Landing";
+import RecentListings from "@/components/home/RecentListings";
 
 const Home = () => {
   return (
     <>
       <Landing />
       <FindInterest />
+      <RecentListings />
+      <FeaturedListings />
     </>
   );
 };
