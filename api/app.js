9e9dbff7dbe04bcb0a4ca5f3c4d172867801f558
Elvis Okumu <okothelvis137@gmail.com>
2025-11-10 11:07:06 +0300

    Add static file serving and logging for production environment.

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ const listingRouter = require("./routes/listingRoute");
 const reviewRouter = require("./routes/reviewRoute");
 const globalErrorHandler = require("./controllers/errorController");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
@@ -14,6 +15,14 @@ app.use(cookieParser());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/listings", listingRouter);
 app.use("/api/v1/reviews", reviewRouter);
+
+// SERVE STATIC FILES IN PRODUCTION
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  );
+}
 
 // GLOBAL ERROR HANDLING MIDDLEWARE
 app.use(globalErrorHandler);
