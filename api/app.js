d2878a9786cc8d0d218d71baaabc3e63081fad3e
Elvis Okumu <okothelvis137@gmail.com>
2025-11-10 12:51:16 +0300

    Fix route handling for serving static files in production.

--- api/app.js
+++ api/app.js
@@ -19,7 +19,7 @@ app.use("/api/v1/reviews", reviewRouter);
 // SERVE STATIC FILES IN PRODUCTION
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/dist")));
-  app.get("*", (req, res) =>
+  app.get("/*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
   );
 }
