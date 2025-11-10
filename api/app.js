c1b94a89f25dcbfac127bd35650b218d28078791
Elvis Okumu <okothelvis137@gmail.com>
2025-11-10 13:12:07 +0300

    Fix static file serving path for production environment.

--- api/app.js
+++ api/app.js
@@ -18,9 +18,9 @@ app.use("/api/v1/reviews", reviewRouter);
 
 // SERVE STATIC FILES IN PRODUCTION
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
   });
 }
 
